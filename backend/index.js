fa23687494e0228a13db8d179d0f01b880b7aad4
Javaroys <javaroys@student.jyu.fi>
2024-11-13 19:36:51 +0200

    render korjaus2

--- backend/index.js
+++ backend/index.js
@@ -89,7 +89,7 @@ app.get("/info/", (request, response) => {
 
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log("server running on PORT " + PORT)
 })
